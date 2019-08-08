6711ab42508a12b2fc948c740578e72b7230e59a
cclancy97 <cclancy@bhcc.edu>
2019-08-08 10:08:29 -0400

    Add auth folder
    - Pair programmed with @alexjcode
    - Added authentication for api

---
+++ assets/scripts/auth/api.js
@@ -0,0 +1,42 @@
+const config = require('../config')
+const store = require('../store')
+const signUp = formData => {
+  return $.ajax({
+    url: config.apiUrl + '/sign-up',
+    method: 'POST',
+    data: formData
+  })
+}
+const signIn = formData => {
+  return $.ajax({
+    url: config.apiUrl + '/sign-in',
+    data: formData,
+    method: 'POST'
+  })
+}
+
+const changePassword = formData => {
+  return $.ajax({
+    url: config.apiUrl + '/change-password',
+    data: formData,
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+const signOut = () => {
+  return $.ajax({
+    url: config.apiUrl + '/sign-out',
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+module.exports = {
+  signIn,
+  signUp,
+  signOut,
+  changePassword
+}
--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://blog-api-82668.herokuapp.com',
   development: 'http://localhost:4741'
 }
 

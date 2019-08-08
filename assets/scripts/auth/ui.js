3bd6b8c29a1f9805e765b7cb28269c5ff0d497fa
cclancy97 <cclancy@bhcc.edu>
2019-08-08 10:59:41 -0400

    Add Auth events and UI
    - Pair programmed w/ @alexjcode

---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,55 @@
+const successMessage = message => {
+  $('#message').text(message)
+  $('#message').removeClass('failure')
+  $('#message').addClass('success')
+  // Clear getFormFields
+  $('form').trigger('reset')
+}
+const failureMessage = message => {
+  $('#message').text(message)
+  $('#message').removeClass('success')
+  $('#message').addClass('failure')
+  // Clear getFormFields
+  $('form').trigger('reset')
+}
+const signUpSuccess = responseData => {
+  successMessage('You signed up successfully!')
+}
+const signUpFailure = responseData => {
+  failureMessage('Sign up failed!')
+}
+const signInSuccess = responseData => {
+  $('#change-password').show()
+  $('#sign-out').show()
+  $('#sign-in').hide()
+  $('#sign-up').hide()
+}
+const signInFailure = () => {
+  failureMessage('Sign in failed!')
+}
+const signOutSuccess = responseData => {
+  successMessage('You signed out successfully!')
+  $('#change-password').hide()
+  $('#sign-out').hide()
+  $('#sign-in').show()
+  $('#sign-up').show()
+}
+const signOutFailure = responseData => {
+  failureMessage('Sign out failed!')
+}
+const changePasswordSuccess = responseData => {
+  successMessage('You changed your password!')
+}
+const changePasswordFailure = responseData => {
+  failureMessage('Password not changed!')
+}
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  signOutSuccess,
+  signOutFailure,
+  changePasswordSuccess,
+  changePasswordFailure
+}
